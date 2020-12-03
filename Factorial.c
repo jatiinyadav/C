98fc8d7f079aa766e87a65da834c16c8fac282ae
jatiinyadav <jatin27yadav@gmail.com>
2020-12-03 11:32:26 +0530

    fact

---
+++ Factorial.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() {
+    // Write C code here
+    printf("Enter the no. you want fact : ");
+    printf("The factorial is %d", fact());
+    return 0;
+}
+int fact(int n)
+{
+    int i;
+    int fact = 1;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        fact = fact*i;
+    }
+    return fact;
+}
